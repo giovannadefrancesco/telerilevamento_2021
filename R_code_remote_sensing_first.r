56edc8ceb8ec92069cd16835977b8c0a81d8619f
giovannadefrancesco <80513580+giovannadefrancesco@users.noreply.github.com>
2021-03-18 18:46:55 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -7,5 +7,22 @@ library(raster)
 #Per Windows
 setwd("C:/lab/")
 
+#Questa funzione serve ad importare un'immagine satellitare
+#infatti, la funzione brick serve per importare tutto il pacchetto elle immagini satellitari
 p224r63_2011 <- brick("p224r63_2011_masked.grd")
+p224r63_2011
+#Questo comando serve per plottare le varie immagini
 plot(p224r63_2011)
+
+# B1: blue
+# B2: green
+# B3: red
+# B4: NIR
+
+#colour change
+cl<-colorRampPalette(c("black","grey","light grey"))(100)
+plot(p224r63_2011, col=cl)
+
+#colour change -> new
+cl<-colorRampPalette(c("blue","green","grey","red","magenta","yellow"))(100)
+plot(p224r63_2011, col=cl)
