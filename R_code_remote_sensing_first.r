a303dee17fe83ecb2b79ee2a80d5aee59fcc3e80
giovannadefrancesco <80513580+giovannadefrancesco@users.noreply.github.com>
2021-03-26 10:52:54 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -15,10 +15,14 @@ p224r63_2011
 #Questo comando serve per plottare le varie immagini
 plot(p224r63_2011)
 
-# B1: blue
-# B2: green
-# B3: red
-# B4: NIR
+# Bande Landsat
+# B1: blu
+# B2: verde
+# B3: rosso
+# B4: infrarosso vicino
+# B5: infrarosso medio
+# B6: infrarosso termico
+# B7: infrarosso medio
 
 #colour change
 cl<-colorRampPalette(c("black","grey","light grey"))(100)
@@ -32,7 +36,7 @@ plot(p224r63_2011, col=cl)
 cl <- colorRampPalette(c("red","pink","orange","purple"))(200)
 plot(p224r63_2011, col=cl)
 
-##DAY 3
+###DAY 3
 
 #dev.off will clean the current graph
 dev.off()
@@ -84,3 +88,43 @@ plot(p224r63_2011$B3_sre, col=clr)
 
 clnir <- colorRampPalette(c("red","orange","yellow")) (100)
 plot(p224r63_2011$B4_sre, col=clnir)
+
+####DAY 4
+#Visualizing data by RGB plotting
+library(raster)
+setwd("C:/lab/")
+
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+# Bande Landsat
+# B1: blu
+# B2: verde
+# B3: rosso
+# B4: infrarosso vicino
+# B5: infrarosso medio
+# B6: infrarosso termico
+# B7: infrarosso medio
+
+#RGB--> Red, Blue, Green, rappresentano i colori primari da cui escono gli altri
+
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+
+#mount a 2X2 multiframe
+pdf("il_mio_primo_pdf.pdf")
+par(mfrow=c(2,2))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+dev.off()
+
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin") #stretch lineare
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist")
+
+#par natural colours, flase colour and false colours with histogram stret
+par(mfrow=c(3,1))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist")
