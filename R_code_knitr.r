db11d76ff2a84e81f72674fb55c4342ae5980a23
giovannadefrancesco <80513580+giovannadefrancesco@users.noreply.github.com>
2021-04-16 12:22:08 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -6,3 +6,6 @@ setwd("C:/lab/") # Windows
 
 # install.packages("knitr")
 library (knitr)
+
+# E' una funzione per la creazione di report automatici su piccola scala basata su uno script R e un modello. 
+stitch("R_code_Greenland.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
