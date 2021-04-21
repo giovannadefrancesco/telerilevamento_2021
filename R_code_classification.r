81cdce512679b4643c97016aa7dc76d086e7f139
giovannadefrancesco <80513580+giovannadefrancesco@users.noreply.github.com>
2021-04-21 12:34:13 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -2,3 +2,35 @@
 
 #Si sceglie la cartella da dove si andranno a leggere i dati
 setwd("C:/lab/") # Windows
+
+#La libreria che ci serve
+library(raster)
+library(RStoolbox)
+#La funzione brick crea un oggetto RasterBrick, il quale è un è un oggetto raster multistrato.
+so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
+
+#Per vedere i livelli
+so
+
+#Per visualizzare i vari livelli si utilizza seguito dal nome dell'immagine
+plotRGB(so, 1, 2, 3, stretch="lin")
+# Si vede un livello dove le esplosioni sono maggiori (a destra), un livello intermedio 
+#e un livello più basso scuro a sinistra.
+
+#UnsuperClass classification
+soc <- unsuperClass(so, nClasses=3)
+plot(soc$map)
+
+# Unsupervised Classification with 20 classes
+soe <- unsuperClass(so, nClasses=20)
+cl <- colorRampPalette(c('blue','red','green'))(100)
+plot(soe$map)
+
+
+# Download an image from:
+# https://www.esa.int/ESA_Multimedia/Missions/Solar_Orbiter/(result_type)/images
+sun <- brick("sun.png")
+
+# Unsupervised classification
+sunc <- unsuperClass(sun, nClasses=3)
+plot(sunc$map)
