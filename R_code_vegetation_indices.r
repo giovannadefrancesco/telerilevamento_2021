fae43d4ecda989692f559abf087a0cedd0715088
giovannadefrancesco <80513580+giovannadefrancesco@users.noreply.github.com>
2021-04-30 10:31:07 +0200

    Update R_code_vegetation_indices.r

--- R_code_vegetation_indices.r
+++ R_code_vegetation_indices.r
@@ -1,7 +1,7 @@
 #R code vegetation indices.r
 
 library(raster)
-
+library(RStoolbox) # for vegetation indices calculation
 setwd("C:/lab/") # Windows
 
 defor1 <- brick("defor1.jpg")
@@ -37,8 +37,38 @@ plot (dvi2)
 
 # specifying a color scheme
 cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
-plot(dvi2, col=cl, main="DVI at time 1")
+plot(dvi2, col=cl, main="DVI at time 2")
 
+par(mfrow=c(2,1))
+plot(dvi1, col=cl, main="DVI at time 1")
+plot(dvi2, col=cl, main="DVI at time 2")
+
+difdvi <- dvi1 - dvi2
+#dev.off()
+cld <- colorRampPalette(c('blue','white','red'))(100) 
+plot(difdvi, col=cld)
+
+#ndvi
+#(NIR-RED)/ (NIR+RED)
+ndvi1 <- (defor1$defor1.1 - defor1$defor1.2) / (defor1$defor1.1 + defor1$defor1.2)
+plot(ndvi1, col=cl)
+
+#Si può utilizzare anche un ltro metodo
+#ndiv <- div1 / (defor1$defor1.1 + defor1$defor2.2 )
+#plot (ndiv2, col=cl)
+
+# RStoolbox::spectralIndices
+vi <- spectralIndices(defor1, green = 3, red = 2, nir = 1)
+plot(vi, col=cl)
+
+
+vi2 <- spectralIndices(defor2, green = 3, red = 2, nir = 1)
+plot(vi2, col=cl)
+
+difndvi <- ndvi1 - ndvi2
+# dev.off()
+cld <- colorRampPalette(c('blue','white','red'))(100) 
+plot(difndvi, col=cld)
 
 
 
