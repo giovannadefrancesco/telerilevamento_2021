4325e20744058a6d988f61d18a3265b609452295
giovannadefrancesco <80513580+giovannadefrancesco@users.noreply.github.com>
2021-05-03 13:00:16 +0200

    Update R_code_multivariate_analysis.r

--- R_code_multivariate_analysis.r
+++ R_code_multivariate_analysis.r
@@ -33,14 +33,27 @@ p224r63_2011res
 #per plottare le due immagini e metterle a confronto
 par(mfrow=c(2,1))
 #immagine 30x30 pixel
-plotRGB(p224r63_2011, r=4, g=3, b=2, stretc="lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="lin")
 #immagine 300x300 pixel ed è la più sgranata 
-plotRGB(p224r63_2011res, r=4, g=3, b=2, stretc="lin")
+plotRGB(p224r63_2011res, r=4, g=3, b=2, stretch="lin")
 
 #La funzione per fare la PCA ("Principal Components Analysis)che consiste nel prendono i  dati originali e passiamo in asse lungo 
 #la variabilità maggiore e l'altro asse lungo la variabilità minore, è rasterPCA-->prende il pacchetto di dati e va a compattarli in un numero minore di bande
 p224r63_2011res_pca <-rasterPCA(p224r63_2011res)
 
+#La funzione summay ci dà un sommario del modello 
+summary(p224r63_2011res_pca$model)
 
+plot(p224r63_2011res_pca$map)
+#da questo plot si ottiene che la prima componente ha tanta deformazione, quindi tanta variabilità mentre l'ultima componente ha il residuo.
 
+#dev.off()
+plotRGB(p224r63_2011res_pca$map, r=1, g=2, b=3, stretch="lin")
+#immagine con tutte e 3 le componenti
+
+#per plottare una singola componente
+plot(p224r63_2011res_pca$map$PC1, p224r63_2011res_pca$map$PC2)
+
+#per vedere tutti i blocchi del file e quali informazioni possono dare sulla struttura informatica del file
+str(p224r63_2011res_pca)
 
