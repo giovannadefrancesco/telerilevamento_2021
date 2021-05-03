5df118354c2f00b05bedb7de3c3abb09728aa076
giovannadefrancesco <80513580+giovannadefrancesco@users.noreply.github.com>
2021-05-03 11:34:23 +0200

    Update R_code_multivariate_analysis.r

--- R_code_multivariate_analysis.r
+++ R_code_multivariate_analysis.r
@@ -5,7 +5,7 @@ library(RStoolbox)
 
 setwd("C:/lab/") # Windows
 
-#Si procede caricando l'immagine con tutte e 7 le bande tramite la funzione brick
+#Si procede caricando l'immagine con tutte e 7 le bande tramite la funzione brick che serve per caricare direttamente un pacchetto di dati
 p224r63_2011 <-brick("p224r63_2011_masked.grd")
 #Per vedere le informazioni delle immagini
 p224r63_2011
@@ -23,3 +23,16 @@ plot(p224r63_2011$B2_sre, p224r63_2011$B1_sre, col="red", pch=19, cex=2)
 #Per plottare tutte le correlazioni possibili fra tutte le variabili del dataset si utilizza la funzione "pairs"
 pairs(p224r63_2011)
 
+#per ridurre la dimensione dell'immagine diminuendo la sua risoluzione si utilizza la funzione aggregate. Resampling-->ricampionamento
+p224r63_2011res <- aggregate(p224r63_2011, fact=10)
+
+#Per vedere le informazioni riguardanti la diminuzione di un fattore 10 il pixel non sarà più di 30m ma di 300m. 
+#Aumentare la grandezza del pixel significa diminuire la risoluzione, maggiore è il dettaglio, più fitta è la risoluzione
+p224r63_2011res
+
+#per plottare le due immagini e metterle a confronto
+par(mfrow=c(2,1))
+#immagine 30x30 pixel
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretc="lin")
+#immagine 300x300 pixel ed è la più sgranata 
+plotRGB(p224r63_2011res, r=4, g=3, b=2, stretc="lin")
